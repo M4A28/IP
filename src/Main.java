e4d1f9bc28b5bf867b710f2c71eff7cd91f93247
mohammedmosapy <mohammed.mosa.eg@gmail.com>
2022-04-21 12:06:04 +0200

    get website IP

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.JTextField;    
 import javax.swing.SwingUtilities;
 
 public class Main extends JFrame implements ActionListener {
